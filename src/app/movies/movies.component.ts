186147a7976fb4bf66f01b64181a69940e2fca5d
Emre GOREN <emregoren98@yandex.com>
2022-07-09 14:14:12 +0200

    some design items added

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -12,4 +12,11 @@ export class MoviesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  movies = [
+    {id:1,title:"Movie 1",description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quibusdam iure reiciendis ab at repudiandae blanditiis quasi, earum similique sunt?",imageUrl:"1.jpeg"},
+    {id:2,title:"Movie 2",description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quibusdam iure reiciendis ab at repudiandae blanditiis quasi, earum similique sunt?",imageUrl:"2.jpeg"},
+    {id:3,title:"Movie 3",description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quibusdam iure reiciendis ab at repudiandae blanditiis quasi, earum similique sunt?",imageUrl:"3.jpeg"},
+    {id:4,title:"Movie 4",description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quibusdam iure reiciendis ab at repudiandae blanditiis quasi, earum similique sunt?",imageUrl:"4.jpeg"},
+]
+
 }
