0daa89a93328eb4f280d7bc827607c19c1f771bd
Emre GOREN <emregoren98@yandex.com>
2022-07-11 01:17:36 +0200

    localize package and some pipes added as comment

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -12,6 +12,7 @@ export class MoviesComponent implements OnInit {
   title:string="Movie List";
   movies:Movie[];
   popularMovies:Movie[];
+  today:Date=new Date("12/02/1998");
 
   movieRepository:MovieRepository;
 
