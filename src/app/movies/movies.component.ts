c93b651471271c72af718005943be6a5abc50635
Emre GOREN <emregoren98@yandex.com>
2022-07-12 01:05:21 +0200

    $event parameter send to method for class arrange

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -12,7 +12,7 @@ export class MoviesComponent implements OnInit {
   title:string="Movie List";
   movies:Movie[];
   popularMovies:Movie[];
-  // today:Date=new Date("12/02/1998");
+  favoriteList:Movie[]=[];
 
   filterText:string="";
 
@@ -28,6 +28,23 @@ export class MoviesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addToList($event:any,movie:Movie){
+    // console.log(movie.title);
+    //console.log($event.target.classList); // When we send to event as a parameter to method, we can use some beneficial things from html page. For Example; we can reach class list,other attrinutes of the html tag.
+    // console.log(event.target);
+
+    if ($event.target.classList.contains("btn-primary")) {
+      $event.target.classList.remove("btn-primary");
+      $event.target.classList.add("btn-danger");
+      $event.target.innerText="Remove from Favorite List";
+    }
+
+    else if ($event.target.classList.contains("btn-danger")) {
+      $event.target.classList.remove("btn-danger");
+      $event.target.classList.add("btn-primary");
+      $event.target.innerText="Add to Favorite List";
+    }
+  }
   
 
 
