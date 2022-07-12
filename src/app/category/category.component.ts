b3b25fd0d2f6ea7a1c18404ec65a86f62dde7752
Emre GOREN <emregoren98@yandex.com>
2022-07-12 11:54:25 +0200

    classes of buttons are connected some conditions

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -12,11 +12,26 @@ export class CategoryComponent implements OnInit {
 
   categoryRepository:CategoryRepository;
   categories:Category[];
+  selectedCategory:Category=null;
+
   constructor() { 
     this.categoryRepository=new CategoryRepository();
     this.categories=this.categoryRepository.getCategories();
   }
   
   ngOnInit(): void {
+  }
+
+  displayAll:boolean=true;
+
+  selectCategory(category?:Category){
+    if (category) {
+      this.selectedCategory=category;
+      this.displayAll=false;
+    }
+    else{
+      this.selectedCategory=null;
+      this.displayAll=true;
+    }
   }
 }
